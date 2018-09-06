08903e80f2ba54765b63319d23e8ae58282a8c2d
Megan Arneau <megan.arneau@ekino.com>
2018-09-06 16:38:01 +0200

    chore: add child routing with the previous routes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { routes } from './pages.routing';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [
+    ProfileComponent,
+    HomeComponent,
+    MessagesComponent,
+    NotFoundComponent
+  ]
 })
 export class PagesModule { }
