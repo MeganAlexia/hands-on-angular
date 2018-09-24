a40dd23fc9143dc14f14be5fc775ada55aaa07c6
Megan Arneau <megan.arneau@ekino.com>
2018-09-24 17:42:31 +0200

    feat: delete unused import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { RouterModule } from '@angular/router';
 import { CoreModule } from './core/core.module';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,10 +16,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     BrowserAnimationsModule,
     CoreModule,
-    MatCardModule,
-    MatToolbarModule,
     RouterModule.forRoot([
       {
         path: '',
